b3104e15ce584724eef5ec33f29dd97cc3d4861d
Eric <ericjiwonchoi@gmail.com>
2014-03-01 21:05:01 -0800

    added 'home' 'about' 'help' static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tweeter::Application.routes.draw do
-  get "static_pages/home"
+   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   resources :microposts
 
   get '/welcome', to: 'welcome#hello'
